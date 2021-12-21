9a2108be442a98166cd8138baad42470da50341a
jongwan <jongbell4@gmail.com>
2021-12-21 22:58:32 +0900

    화면 수정ㅇ

--- src/main/java/com/springboot/shoppingMall/domain/products/dto/ProductDetails.java
+++ src/main/java/com/springboot/shoppingMall/domain/products/dto/ProductDetails.java
@@ -14,12 +14,14 @@ public class ProductDetails {
     private String name;
     private String filePath;
     private int price;
+    private int stockQuantity;
 
     public ProductDetails(Product entity){
         this.productId = entity.getId();
         this.name = entity.getName();
         this.filePath = entity.getFilePath();
         this.price = entity.getPrice();
+        this.stockQuantity = entity.getStockQuantity();
     }
 
 
