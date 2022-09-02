c1f89379c15d4a20394b4c35abca983b0dd3b11c
JK-Kim4 <jongbell4@gmail.com>
2022-09-02 10:40:26 +0900

    - Admin page template 작업
    DATE: 2022.09.02
    AUTHOR: KJW

--- src/main/java/com/springboot/shoppingMall/IndexController.java
+++ src/main/java/com/springboot/shoppingMall/IndexController.java
@@ -3,10 +3,16 @@ package com.springboot.shoppingMall;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    @GetMapping(value = {"/index", "/"})
+    public String index(){
+        return "/admin/index";
+    }
 
 }
